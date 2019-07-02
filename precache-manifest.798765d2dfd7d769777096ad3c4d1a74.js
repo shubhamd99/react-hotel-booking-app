210bf7c0614006a4011905244f02c65fa455d8fb
Shubham Dhage <shubhamdhage930@gmail.com>
2019-07-02 22:43:02 +0530

    Updates

---
+++ precache-manifest.798765d2dfd7d769777096ad3c4d1a74.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2430ff67ba7092d2283eab2e08c1c227",
+    "url": "/react-hotel-booking-app/index.html"
+  },
+  {
+    "revision": "65a2266b8f01fd5075f2",
+    "url": "/react-hotel-booking-app/static/css/main.479488ba.chunk.css"
+  },
+  {
+    "revision": "9c90d3095d4240c17f97",
+    "url": "/react-hotel-booking-app/static/js/2.8d8ceb85.chunk.js"
+  },
+  {
+    "revision": "65a2266b8f01fd5075f2",
+    "url": "/react-hotel-booking-app/static/js/main.09842a46.chunk.js"
+  },
+  {
+    "revision": "e7396f5fa1a039af40b0",
+    "url": "/react-hotel-booking-app/static/js/runtime~main.9e9632cf.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/react-hotel-booking-app/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/react-hotel-booking-app/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/react-hotel-booking-app/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/react-hotel-booking-app/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/react-hotel-booking-app/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/react-hotel-booking-app/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "c7c76c7a194983fa489c0e1b2d48dd3b",
+    "url": "/react-hotel-booking-app/static/media/logo.c7c76c7a.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/react-hotel-booking-app/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/react-hotel-booking-app/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/react-hotel-booking-app/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/react-hotel-booking-app/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/react-hotel-booking-app/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/react-hotel-booking-app/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/react-hotel-booking-app/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/react-hotel-booking-app/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/react-hotel-booking-app/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/react-hotel-booking-app/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/react-hotel-booking-app/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/react-hotel-booking-app/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
